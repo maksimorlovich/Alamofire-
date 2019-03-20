2dd4bb412fd938d83abd664a3cb61c7f4bd87212
Maksim Orlovich <maksim.orlovich@gmail.com>
2019-03-20 15:40:18 -0500

    Update Package.swift to point to Alamofire Linux branch

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.4.0"),
-        .package(url: "https://github.com/maksimorlovich/Alamofire.git", .branch("alamofire5-linux")),
+        .package(url: "https://github.com/maksimorlovich/Alamofire.git", .branch("alamofire-linux")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
